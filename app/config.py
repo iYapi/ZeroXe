98bc59188bdba13a0adcad580d32984c7b4c1c05
russianwaifu <yafialhaidar18@gmail.com>
2026-07-01 13:56:27 +0700

    pre-release: v0.0.16 beta

--- app/config.py
+++ app/config.py
@@ -18,7 +18,7 @@ def get_config_dir(app_name="myapp") -> str:
 
 class Settings:
     APP_NAME = "ZerØXe"
-    BUILD_VERSION = "v0.0.15"
+    BUILD_VERSION = "v0.0.16"
 
     VERSIONING_FOLDER="progress"
     VERSIONING_LOG_FOLDER=".zeroxe"
